fa84cc369acb52b99b6e88d89de9240f9d2ea1f0
grozovoy <lflorezmatute@gmail.com>
2020-02-27 12:52:59 +0100

    Crear paquete dao e interfaz PerfilDao
    Modificado pom.xml con dependencias de jpa

---
+++ lucaTinder/src/main/java/com/luca/tinder/dao/PerfilDao.java
@@ -0,0 +1,11 @@
+package com.luca.tinder.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.luca.tinder.model.Perfil;
+
+@Repository
+public interface PerfilDao extends JpaRepository<Perfil, Integer>{
+
+}
