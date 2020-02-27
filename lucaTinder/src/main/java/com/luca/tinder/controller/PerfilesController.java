a190e4f3977477e10b1fa43de0de6df057b572da
David <davidheras19@gmail.com>
2020-02-27 14:20:52 +0100

    CREADO metodo savePerfil en la clase PerfilesController

--- lucaTinder/src/main/java/com/luca/tinder/controller/PerfilesController.java
+++ lucaTinder/src/main/java/com/luca/tinder/controller/PerfilesController.java
@@ -6,6 +6,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.luca.tinder.model.Perfil;
 
@@ -13,11 +16,24 @@ import com.luca.tinder.model.Perfil;
 public class PerfilesController {
 
 	private static final Logger logger=LoggerFactory.getLogger(PerfilesController.class);
+	
 	@GetMapping("/new")
 	public String createNewPerfil(ModelMap model) {
 		logger.info("-- en NEW");
 		model.addAttribute("perfil", new Perfil());
 		return "PerfilForm";
+		
+	}
+	
+	
+	
+	@PostMapping("/save")
+	public ModelAndView savePerfil(@ModelAttribute Perfil perfil) {
+		logger.info("-- en SAVE");
+		
+		perfilService.add(perfil);
+		return new ModelAndView("redirect:/");
+		
 		
 	}
 }
