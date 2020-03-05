7a031c678d7bfa8c60150743ab6f2258a98bacb1
David <davidheras19@gmail.com>
2020-03-05 18:31:42 +0100

    creada prueba junit

--- lucaTinder/src/test/java/com/luca/tinder/LucaTinderApplicationTests.java
+++ lucaTinder/src/test/java/com/luca/tinder/LucaTinderApplicationTests.java
@@ -1,13 +1,25 @@
 package com.luca.tinder;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.luca.tinder.controller.PerfilesController;
+
 
 @SpringBootTest
 class LucaTinderApplicationTests {
 
+	@Autowired
+    private PerfilesController controller;
+	
 	@Test
 	void contextLoads() {
+		assertThat(controller).isNotNull();
+	
+	
 	}
 
 }
