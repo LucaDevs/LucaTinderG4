755a9e8eb31070402a65e42c01c511849dd8af02
grozovoy <lflorezmatute@gmail.com>
2020-03-05 17:59:19 +0100

    Añadidos javadoc para CategoriaService

--- lucaTinder/src/main/java/com/luca/tinder/service/CategoriaService.java
+++ lucaTinder/src/main/java/com/luca/tinder/service/CategoriaService.java
@@ -3,7 +3,11 @@ package com.luca.tinder.service;
 import java.util.List;
 
 import com.luca.tinder.model.Categoria;
-
+/**
+ * 
+ * @author David Heras
+ *
+ */
 public interface CategoriaService {
 
 	List<Categoria> listarCategoria();
